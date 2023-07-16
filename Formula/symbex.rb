bbad4ef337061e54ed92a03687ee4dea20ae7e4b
Automated <actions@users.noreply.github.com>
2023-07-16 01:19:42 +0000

    Update symbex: Sun Jul 16 01:19:42 UTC 2023

--- Formula/symbex.rb
+++ Formula/symbex.rb
@@ -3,14 +3,14 @@ class Symbex < Formula
 
   desc "Find the Python code for specified symbols"
   homepage "https://github.com/simonw/symbex"
-  url "https://files.pythonhosted.org/packages/87/97/27e5cfb680f749fa6b7a8a7db16517d2bd3ca4b933250b3c004b45b77668/symbex-1.0.tar.gz"
-  sha256 "579564cfdf077935c924daead5b06af85f358c7250fb5b7dbc61685a296264a9"
+  url "https://files.pythonhosted.org/packages/4e/72/197389866706eb6d75f9313391de6f331d0bf90ee6dd4fae781c60cc2b99/symbex-1.1.tar.gz"
+  sha256 "df1f0685366ed5938235dd130b0343fc55bac91f111069a55f89de2a3ea5b800"
 
   depends_on "python3"
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
-    sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
+    url "https://files.pythonhosted.org/packages/7e/ad/7a6a96fab480fb2fbf52f782b2deb3abe1d2c81eca3ef68a575b5a6a4f2e/click-8.1.5.tar.gz"
+    sha256 "4be4b1af8d665c6d942909916d31a213a106800c47d0eeba73d34da3cbc11367"
   end
 
   def install
